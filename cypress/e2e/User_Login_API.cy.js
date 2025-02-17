d1e3fa8b707a1c7e503f6c5ec485e02ac0290362
Phil <pheel@live.ca>
2025-02-17 13:05:14 -0500

    Passing values between steps

--- cypress/e2e/User_Login_API.cy.js
+++ cypress/e2e/User_Login_API.cy.js
@@ -1,15 +1,31 @@
 /// <reference types="cypress" />
 
-describe('Login an existing user using the API', () => {
-    it('Logs in a user', () => {
+describe('Login with an existing user and get its information', () => {
+    it('Submits the user information and receives a token', () => {
       cy.request('POST', "https://thinking-tester-contact-list.herokuapp.com/users/login", {
         "email":"johndodo@test.com",
         "password":"test123"
-      }).then((response) => {
+      }).should((response) => {
         expect(response).property('status').to.equal(200)
         expect(response).property('body').to.have.property('token')
-        expect(response.body.token).to.match(new RegExp("^[A-Za-z0-9-_=]+\\.[A-Za-z0-9-_=]+\\.?[A-Za-z0-9-_.+/=]*$"))
+        expect(response.body.token).to.match(new RegExp("^[A-Za-z0-9-_=]+\\.[A-Za-z0-9-_=]+\\.?[A-Za-z0-9-_.+/=]*$"))        
+      }).then((response) => {
+        Cypress.env('token', response.body.token)
       })
     })
   
+    it('Gets the user information using a token', () => {
+      cy.request({
+        url:'https://thinking-tester-contact-list.herokuapp.com/users/me',
+        headers: {
+          Authorization: 'Bearer ' + Cypress.env('token')
+        }})
+      .should((response) => {
+        expect(response).property('status').to.equal(200)
+        expect(response).property('body').to.have.property('firstName')
+        expect(response).property('body').to.have.property('lastName')
+        expect(response).property('body').to.have.property('email')
+        expect(response.body.email).to.equal("johndodo@test.com")
+      })
+    })
   })
